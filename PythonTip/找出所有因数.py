e085303b3788aa6250cdaa4ac956311d5313fd4e
Yabing67 <wybing0607@gmail.com>
2024-03-07 22:21:35 +0100

    Create 找出所有因数.py

---
+++ PythonTip/找出所有因数.py
@@ -0,0 +1,20 @@
+def find_factors(number):
+    factors = []
+    # 特殊处理 1
+    factors.append(1)
+    # 遍历从 2 到 sqrt(number) 的所有数字
+    i = 2
+    while i * i <= number:
+        # 如果 number 能够整除 i，则 i 和 number/i 都是 number 的因数
+        if number % i == 0:
+            factors.append(i)
+            if number // i != i:
+                factors.append(number // i)
+        i += 1
+    # 对因数列表进行排序并返回
+    return sorted(factors)
+
+# 示例输入
+number = 16
+# 调用函数并打印结果
+print("Factors of", number, "are:", find_factors(number))
